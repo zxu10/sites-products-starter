4c18e9296b9e8c45ea66f21a04dab00bef04d954
Luc Yuki Marrie <lucmarrie1@gmail.com>
2022-01-18 16:46:57 -0500

    Updated ci_config for hbsTemplates

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
 import './global.css';
-
-function importAll(r) {
-    let images = {};
-    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
-    return images;
-  }
-  
-  const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@ module.exports = {
   mode: "jit",  
   purge: [
       './partials/**/*.hbs', 
-      './pages/**/*.hbs',
       './templates/**/*.hbs',
     ],
     darkMode: false, // or 'media' or 'class'
